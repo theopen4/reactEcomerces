a803b94f2685524c591c55876feb27825c26650c
theopen4 <kielze86@gmail.com>
2023-05-22 17:02:03 +0100

    git init repository

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <header>
-        <a href="/">DSKE</a>
+        <a href="/">DSKE AMAZON</a>
       </header>
       <main>list product</main>
     </div>
