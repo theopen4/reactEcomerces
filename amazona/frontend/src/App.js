cf94bddd6a7a2e91746e7ac543442d81df326c0a
theopen4 <kielze86@gmail.com>
2023-05-26 15:14:43 +0100

    Fetch Products From Backend

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
