2f3cfac73069b7937cfd4163919c54d426d37f90
theopen4 <kielze86@gmail.com>
2023-06-13 23:17:49 +0100

    task bootsrap

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link href="/"> amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>DSKE AMAZON</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
