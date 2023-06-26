9c18ef58d3a05f088f173461a851db0e445d47e5
theopen4 <kielze86@gmail.com>
2023-06-26 21:23:21 +0100

    11-create rating and product

--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -1,9 +1,12 @@
 import React, { useReducer } from 'react';
 //import data from '../data';
-import { Link } from 'react-router-dom';
+
 import axios from 'axios';
 import { useEffect } from 'react';
 import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -48,22 +51,13 @@ function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
